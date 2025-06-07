01b0b5587c946cfb67bf3ebd642f00ea4d5a14a7
Wiktor Owczarek <tikolu43@gmail.com>
2025-06-07 10:41:27 +0100

    Support TSV (tab-separated values)

--- submit-names.js
+++ submit-names.js
@@ -7,9 +7,13 @@ function importData() {
     // Parse CSV
     const passengerData = []
     const csvRows = csv.split("\n")
+    let splitChar = ""
+    if(csvRows[0].includes(",")) splitChar = ","
+    else if(csvRows[0].includes("\t")) splitChar = "\t"
+    else throw "Couldn't determine CSV delimeter. Ensure data is comma or tab separated"
     for(const rowIndex in csvRows) {
         const csvRow = csvRows[rowIndex]
-        const csvData = csvRow.split(",").map(d => d.trim()).filter(d => d)
+        const csvData = csvRow.split(splitChar).map(d => d.trim()).filter(d => d)
         if(csvData.length != 4) throw `Invalid CSV row ${Number(rowIndex) + 1}: ${csvRow}`
         passengerData.push(csvData)
     }
@@ -75,4 +79,11 @@ function importData() {
         importCount += 1
     }
     alert(`Successfully imported ${importCount} of ${passengerData.length} entries`)
+}
+
+try {
+    importData()
+} catch(error) {
+    console.error(error)
+    alert(error)
 }
